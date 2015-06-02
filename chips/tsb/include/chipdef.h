d94d3df7a30397e1c6cb0dbb120cee801ff2acda
Jun Li <li_jun@projectara.com>
2015-06-02 11:07:09 -0700

    Resume from standby
    This change contains:
      1. Communication area allocation per the boot ROM spec, including where the
         resume address is stored.
      2. Resume from standby code.
      3. Last time, forgot to define the chip revision for ES1. Now added it since
         reading the register BOOTRET_O would hang the system on ES1.
    
    Testing Done: The code to resume to address stored in the communication area is
                  tested. However, the code that gets workram out of retention mode
                  is NOT tested due to limitation of the FPGA bits. I just coded as
                  Toshiba's spec said but have no idea if it would work or not. We
                  may need Toshiba to do some simulation to verify that part.
    Bug Number:   SW-723
    Review URL:   https://reviews.leaflabs.com/r/799/
    Reviewed by:  Fabien Parent <fparent@baylibre.com>,
                  Morgan Girling <girling_morgan@projectara.com>
    
    Signed-off-by: Jun Li <li_jun@projectara.com>

---
+++ chips/tsb/include/chipdef.h
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2015 Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ARCH_ARM_TSB_CHIPDEF_H
+#define __ARCH_ARM_TSB_CHIPDEF_H
+
+/* chip revision:
+    higher 4bits are production chip revision, so it is 0 for all ESx.
+    lower 4bits are minor revisions, here is stands for the x in ESx
+*/
+#define CHIP_REVISION_ES1 0x01
+#define CHIP_REVISION_ES2 0x02
+#define CHIP_REVISION_ES3 0x03
+
+/* AP/GP Bridge peripherals memory map */
+
+#define SYSCTL_BASE     0x40000000
+#define SYSCTL_SIZE     0x1000
+
+#define I2C_BASE        0x40001000
+#define I2C_SIZE        0x100
+
+#define TMR_BASE        0x40002000
+#define TMR_SIZE        0x800
+
+#define GPIO_BASE       0x40003000
+#define GPIO_SIZE       0x40
+
+#define PWMOD_BASE      0x40004000
+#define PWMOD_SIZE      0x400
+
+#define UART_BASE       0x40005000
+#define UART_SIZE       0x100
+
+#define MBOX_BASE       0x40006000
+#define MBOX_SIZE       0x1000
+
+#define I2SLP_SC_BASE   0x40007000
+#define I2SLP_SC_SIZE   0x100
+
+#define I2SLP_SO_BASE   0x40008000
+#define I2SLP_SO_SIZE   0x200
+
+#define I2SLP_SI_BASE   0x40009000
+#define I2SLP_SI_SIZE   0x200
+
+#define CDSI0_BASE      0x40010000
+#define CDSI0_SIZE      0x2000
+
+#define CDSI1_BASE      0x40012000
+#define CDSI1_SIZE      0x2000
+
+#define GDMAC_BASE      0x40014000
+#define GDMAC_SIZE      0x1000
+
+#define GDMACN_BASE     0x40015000
+#define GDMACN_SIZE     0x1000
+
+#define SPI_BASE        0x40018000
+#define SPI_SIZE        0x800
+
+#define UHSSD_BASE      0x40019000
+#define UHSSD_SIZE      0x100
+
+#define AIO_UNIPRO_BASE 0x40020000
+#define AIO_UNIPRO_SIZE 0x10000
+
+#define HSIC_BASE       0x40040000
+#define HSIC_SIZE       0x1000
+
+#define CM3UP_BASE      0xE000E000
+#define CM3UP_SIZE      0x1000
+
+#define PMU_BASE        0x40090000
+#define PMU_SIZE        64
+
+/* PMU registers */
+#define RETSRAMRET      (PMU_BASE + 0x10)
+#define RETSRAMCENCONT  (PMU_BASE + 0x14)
+#define RETSRAMCLKCONT  (PMU_BASE + 0x18)
+#define BOOTRET_O       (PMU_BASE + 0x2C)
+
+#endif /* __ARCH_ARM_TSB_CHIPDEF_H */
