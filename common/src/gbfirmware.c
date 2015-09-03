9b6f2ffc39f058c612fc8944d1ae38ba3c602974
Eli Sennesh <esennesh@leaflabs.com>
2015-09-03 17:34:06 -0400

    Greybus Firmware driver: don't send stage in ready-to-boot request
    The Greybus Firmware Specification no longer requires us to pass the coming
    boot stage as part of a ready-to-boot request, so don't.
    
    Testing Done: brought up firmware on apb2
    Bug Number: Trivial
    Review URL: https://reviews.leaflabs.com/r/1558/
    Reviewed by: Morgan Girling <girling_morgan@projectara.com>
    
    Signed-off-by: Eli Sennesh <esennesh@leaflabs.com>

--- common/src/gbfirmware.c
+++ common/src/gbfirmware.c
@@ -136,7 +136,7 @@ static int gbfw_get_firmware_response(gb_operation_header *header, void *data,
 
 static int gbfw_ready_to_boot(uint8_t status) {
     int rc;
-    struct gbfw_ready_to_boot_request req = {NEXT_BOOT_STAGE, status};
+    struct gbfw_ready_to_boot_request req = {status};
     rc = greybus_send_request(gbfw_cportid, 1, GB_FW_OP_READY_TO_BOOT,
                               (uint8_t*)&req, sizeof(req));
     if (rc) {
