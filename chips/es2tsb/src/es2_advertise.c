projectara/bootrom

commit c3fcac7a9110782fee4645b5ad97972ce2baf206

Update after code review
This change contains:
1. make boot_status_offline static to chip_advertise_boot_status.
2. check paramter in chip_is_key_revoked.

Testing Done:   still work as expected
Bug Number:     SW-1544
Review URL:     https://reviews.leaflabs.com/r/1926/
Reviewed by:    Morgan Girling <girling_morgan@projectara.com>

Signed-off-by:  Jun Li <li_jun@projectara.com>

chips/es2tsb/src/es2_advertise.c
@@ -26,6 +26,7 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 #include "chip.h"
 #include "chipapi.h"
@@ -35,8 +36,6 @@
 #include "error.h"
 #include "utils.h"
 
-uint32_t boot_status_offline = 0;
-
 /**
  * @brief advertise the boot status
  * @param boot_status
@@ -45,6 +44,10 @@ uint32_t boot_status_offline = 0;
  */
 void chip_advertise_boot_status(uint32_t boot_status) {
     int rc;
+    static bool boot_status_offline = false;
+
+    if (!boot_status_offline)
+        return;
 
     rc = chip_unipro_attr_write(T_TSTSRCINCREMENT, ES2_INIT_STATUS(boot_status),
                                 0, ATTR_LOCAL);
@@ -53,8 +56,8 @@ void chip_advertise_boot_status(uint32_t boot_status) {
      */
     if (rc) {
         /*
-         * Set this flag so that halt_and_catch_fire doesn't try to
-         * recursively call us to advertise the boot status.
+         * Set this flag so we can exit the recursive call from
+         * halt_and_catch_fire
          */
         boot_status_offline = true;
         halt_and_catch_fire(boot_status);

chips/es3tsb/src/es3_advertise.c
@@ -26,6 +26,7 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 #include "chip.h"
 #include "unipro.h"
@@ -33,8 +34,6 @@
 #include "chipapi.h"
 #include "error.h"
 #include "bootrom.h"
-
-uint32_t boot_status_offline = 0;
 
 /**
  * @brief advertise the boot status
@@ -44,7 +43,11 @@ uint32_t boot_status_offline = 0;
  *         status
  */
 void chip_advertise_boot_status(uint32_t boot_status) {
+    static bool boot_status_offline = false;
     int status = 0;
+
+    if (!boot_status_offline)
+        return;
 
     status = chip_unipro_attr_write(DME_DDBL2_INIT_STATUS, boot_status, 0,
                                     ATTR_LOCAL);
@@ -53,8 +56,8 @@ void chip_advertise_boot_status(uint32_t boot_status) {
      */
     if (status != 0) {
         /*
-         * Set this flag so that halt_and_catch_fire doesn't try to
-         * recursively call us to advertise the boot status.
+         * Set this flag so we can exit the recursive call from
+         * halt_and_catch_fire
          */
         boot_status_offline = true;
         halt_and_catch_fire(boot_status);

common/include/bootrom.h
@@ -39,6 +39,7 @@
  */
 extern uint32_t ara_vid;
 extern uint32_t ara_pid;
+
 /* This is set to non-zero if chip_advertise_boot_status can't write to DME) */
 extern uint32_t boot_status_offline;
 

common/src/error.c
@@ -174,9 +174,7 @@ void halt_and_catch_fire(uint32_t boot_status) {
     dbgflush();
     /* NOTE: NO FURTHER DEBUG MESSAGES BETWEEN HERE AND FUNCTION END! */
 
-    if (!boot_status_offline) {
-        chip_advertise_boot_status(boot_status);
-    }
+    chip_advertise_boot_status(boot_status);
 
     /*
      * Indicate failure with GPIO 18 showing a '1' and execute a handshake

chips/tsb/src/tsb_isaa.c
@@ -230,11 +230,17 @@ void tsb_jtag_disable(void) {
     isaa_write(TSB_ISAA_JTAG_DISABLE, TSB_JTAG_DISABLE);
 }
 
-int chip_is_key_revoked(int index) {
+int chip_is_key_revoked(uint32_t index) {
+    uint32_t bit = (1 << index);
+
+    if ((bit & TSB_ISAA_ROM_KEY_VALIDITY) == 0) {
+        return 1;
+    }
+
 #if CONFIG_CHIP_REVISION >= CHIP_REVISION_ES3
     uint32_t scr = isaa_read(TSB_ISAA_SCR);
 
-    if ((scr & (1 << index)) != 0) {
+    if ((scr & bit) != 0) {
         return 1;
     }
 #endif

common/include/chipapi.h
@@ -153,7 +153,7 @@ void chip_reset_before_ready(void);
  * @return 0 indicates the key is NOT revoked
  *         1 indicates the key has been revoked so should not be used.
  */
-int chip_is_key_revoked(int index);
+int chip_is_key_revoked(uint32_t index);
 
 /*
  * @brief wait for UniPro link up sequence to finish

common/include/string.h
@@ -35,4 +35,6 @@ void *memcpy(void *dest, const void *src, size_t n);
 
 void *memset(void *s, int c, size_t n);
 
+int strncmp(const char *s1, const char *s2, size_t n);
+
 #endif /* __COMMON_INCLUDE_STRING_H */

common/src/crypto.c
@@ -90,26 +90,21 @@ void hash_final(unsigned char *digest) {
 }
 
 static int find_public_key(tftf_signature *signature, const unsigned char **key) {
-    uint32_t *ps, *pk;
-    int i, k;
-    uint32_t size = (sizeof(public_keys[0]) -
-                     sizeof(public_keys[0].key)) /
-                    sizeof(uint32_t);
-
-    ps = (uint32_t *)&(signature->type);
+    uint32_t k;
 
     for (k = 0; k < number_of_public_keys; k++) {
         if (chip_is_key_revoked(k)) {
             dbgprintx32("Key ", k, " revoked\n");
             continue;
         }
-        pk = (uint32_t *)&public_keys[k];
-        for (i = 0; i < size; i++) {
-            if (ps[i] != pk[i]) {
-                break;
-            }
+
+        if (public_keys[k].type != signature->type) {
+            continue;
         }
-        if (i >= size) {
+
+        if (!strncmp(public_keys[k].key_name,
+                     signature->key_name,
+                     sizeof(public_keys[k].key_name))) {
             dbgprint("Found pub. key\n");
             *key = public_keys[k].key;
             return 0;
@@ -130,10 +125,10 @@ static int find_public_key(tftf_signature *signature, const unsigned char **key)
  */
 int verify_signature(unsigned char *digest, tftf_signature *signature) {
 #ifdef _NOCRYPTO
-/* little ending 32bit word for "FAIL" */
-#define _SIM_KEYNAME_FAILURE_SENTINEL 0x4C494146
-    uint32_t *pname = (uint32_t *)(signature->key_name);
-    if (*pname == _SIM_KEYNAME_FAILURE_SENTINEL) {
+#define _SIM_KEYNAME_FAILURE_SENTINEL "no_crypto_fake_failure_key"
+    if (strncmp(signature->key_name,
+                _SIM_KEYNAME_FAILURE_SENTINEL,
+                sizeof(signature->key_name))) {
         return -1;
     }
     return 0;

commit e58c7d7ede1bfef7a5590e2061fbefb811591efa

es2/es3: Fix broken boot-status advertise
With the introduction of c3fcac7a9110 ("Update after code review"), we
never advertise the boot status as boot_status_offline stays false for
ever. chip_advertise_boot_status() returns early by checking the same
variable, it is supposed to update.

And so we never advertise the boot status, and nothing proceeds further.

Testing Done: Tested boot over unipro on DB3 with real module
Bug Number: Trivial
Review URL: https://reviews.leaflabs.com/r/2104/
Reviewed by: Eli Sennesh

Fixes: c3fcac7a9110 ("Update after code review")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

chips/es2tsb/src/es2_advertise.c
@@ -36,7 +36,7 @@
 #include "error.h"
 #include "utils.h"
 
-static bool boot_status_offline = false;
+bool boot_status_offline = false;
 
 /**
  * @brief advertise the boot status
@@ -46,9 +46,6 @@ static bool boot_status_offline = false;
  */
 void chip_advertise_boot_status(uint32_t boot_status) {
     int rc;
-
-    if (!boot_status_offline)
-        return;
 
     rc = chip_unipro_attr_write(T_TSTSRCINCREMENT, ES2_INIT_STATUS(boot_status),
                                 0, ATTR_LOCAL);

chips/es3tsb/src/es3_advertise.c
@@ -35,7 +35,7 @@
 #include "error.h"
 #include "bootrom.h"
 
-static bool boot_status_offline = false;
+bool boot_status_offline = false;
 
 /**
  * @brief advertise the boot status
@@ -46,9 +46,6 @@ static bool boot_status_offline = false;
  */
 void chip_advertise_boot_status(uint32_t boot_status) {
     int status = 0;
-
-    if (!boot_status_offline)
-        return;
 
     status = chip_unipro_attr_write(DME_DDBL2_INIT_STATUS, boot_status, 0,
                                     ATTR_LOCAL);

common/include/bootrom.h
@@ -40,6 +40,8 @@
 extern uint32_t ara_vid;
 extern uint32_t ara_pid;
 
+extern bool boot_status_offline;
+
 int locate_ffff_element_on_storage(data_load_ops *ops,
                                    uint32_t type,
                                    uint32_t *length);

common/src/error.c
@@ -178,7 +178,8 @@ void halt_and_catch_fire(uint32_t boot_status) {
     dbgflush();
     /* NOTE: NO FURTHER DEBUG MESSAGES BETWEEN HERE AND FUNCTION END! */
 
-    chip_advertise_boot_status(boot_status);
+    if (!boot_status_offline)
+        chip_advertise_boot_status(boot_status);
 
     /*
      * Indicate failure with GPIO 18 showing a '1' and execute a handshake