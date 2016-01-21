6f5dfea6c61ae3ee6def8ec3fca99e04c9abd69c
Jun Li <li_jun@projectara.com>
2016-01-21 13:59:17 -0800

    Build two different Elliptic curves
    In the plan, two different Elliptic curves could be used in S2L.
    Config the make file to build MIRACL library for two curves.
    
    Testing Done:   Build does not break
    Bug Number:     Trivial
    Review URL:     https://reviews.leaflabs.com/r/2668/
    Reviewed by:    Eli Sennesh, Morgan Girling
    
    Signed-off-by:  Jun Li <li_jun@projectara.com>

--- apps/secondstage/MIRACL_cfg/config.mk
+++ apps/secondstage/MIRACL_cfg/config.mk
@@ -23,11 +23,24 @@ MCL_C448:=5
 # Note not all curve options are supported on 16-bit processors - see rom.c
 MCL_CHUNK:=32
 # Current choice of Elliptic Curve (see mcl_arch.h)
-MCL_CHOICE:=$(MCL_C25519)
+# The value defined here is not important. It will be replaced by the value
+# passed in from the make command line.
+MCL_CHOICE:=$(MCL_C448)
+
 # type of curve  (see mcl_arch.h) 
 MCL_CURVETYPE:=$(MCL_EDWARDS)
 # 2^n multiplier of MCL_BIGBITS to specify supported Finite Field size, 
 # e.g 2048=256*2^3 where MCL_BIGBITS=256  (see mcl_config.h) 
 MCL_FFLEN:=8
 
+# Application specific configs to build multiple curves.
+DREC1=C448
+DREC2=C25519
+
+DRRSA1=RSA2048
+# We only use RSA2048. DRRSA2 and DRRSA3 are defined to pass compiling
+# DRRSA2 would still be compiled into the libmclcurve$(DREC2).a but will
+# not be in the final image since no one is going to call it.
+DRRSA2=RSANA
+DRRSA3=RSA3072
 
